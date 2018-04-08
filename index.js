b051f186e318cf9fd7a63425b03acb9f8c730c75
Ch3sh1r3Cat <ca.leduc@me.com>
2018-04-08 23:39:19 +0200

    Version 0.1.4
    - Compiled index.js with Babel to add compatibility with IE11 or older

--- index.js
+++ index.js
@@ -3,90 +3,95 @@
  * Copyright (c) 2018 Charles-André LEDUC. All rights reserved.
  */
 
-const OnLoad = {
-  install(Vue, options) {
-    var _images = {}
-    var classLoading = 'loading'
-    var classLoaded = 'loaded'
+var OnLoad = {
+  install: function install(Vue, options) {
+    var _images = {};
+    var classLoading = 'loading';
+    var classLoaded = 'loaded';
     if (options) {
-      if (options.classLoading) classLoading = options.classLoading
-      if (options.classLoaded) classLoaded = options.classLoaded
+      if (options.classLoading) classLoading = options.classLoading;
+      if (options.classLoaded) classLoaded = options.classLoaded;
     }
 
-    const imgFn = {
-      load: function(filename, callback) {
-        const _img = new Image()
-        _img.onload = function() {
-          _img.width = this.width
-          _img.height = this.height
-          _images[filename] = _img
+    var imgFn = {
+      load: function load(filename, callback) {
+        var _img = new Image();
+        _img.onload = function () {
+          _img.width = this.width;
+          _img.height = this.height;
+          _images[filename] = _img;
           if (callback) {
-            callback(_img)
+            callback(_img);
           }
-        }
-        _img.alt = filename
-        _img.src = require('@/assets/img/' + filename)
+        };
+        _img.alt = filename;
+        _img.src = require('@/assets/img/' + filename);
       },
 
-      set: function(source, target) {
-        target.src = source.src
-        target.width = source.width
-        target.height = source.height
-        target.classList.toggle(classLoading, false)
-        target.classList.toggle(classLoaded, true)
+      set: function set(source, target) {
+        target.src = source.src;
+        target.width = source.width;
+        target.height = source.height;
+        target.classList.toggle(classLoading, false);
+        target.classList.toggle(classLoaded, true);
       }
-    }
+    };
 
-    const directiveFn = function(el, binding) {
-      const name = binding.value
-      var source = _images[name]
+    var directiveFn = function directiveFn(el, binding) {
+      var name = binding.value;
+      var source = _images[name];
       if (source != null) {
-        imgFn.set(source, el)
+        imgFn.set(source, el);
       } else {
-        el.classList.toggle(classLoading, true)
-        el.classList.toggle(classLoaded, false)
-        imgFn.load(name, function(img) {
-          imgFn.set(img, el)
-        })
+        el.classList.toggle(classLoading, true);
+        el.classList.toggle(classLoaded, false);
+        imgFn.load(name, function (img) {
+          imgFn.set(img, el);
+        });
       }
-    }
+    };
 
     Vue.directive('onload', {
-      inserted: function(el, binding, vnode) {
+      inserted: function inserted(el, binding, vnode) {
         if (vnode.tag !== 'img') {
-          console.warn('Current node is not an image!')
-          return
+          console.warn('Current node is not an image!');
+          return;
         }
-        directiveFn(el, binding)
+        directiveFn(el, binding);
       },
-      update: function(el, binding, vnode, oldVnode) {
+      update: function update(el, binding, vnode, oldVnode) {
         if (vnode.tag !== 'img') {
-          console.warn('Current node is not an image!')
-          return
+          console.warn('Current node is not an image!');
+          return;
         }
-        directiveFn(el, binding)
+        directiveFn(el, binding);
       }
-    })
+    });
 
     Vue.prototype.$images = {
-      preload: function(images, callback) {
-        const l = images.length
-        var n = 0
-        for (var i in images) {
-          const name = images[i]
-          imgFn.load(name, function(img) {
-            n++
-            _images[name] = img
+      preload: function preload(images, callback) {
+        var l = images.length;
+        var n = 0;
+
+        var _loop = function _loop() {
+          var name = images[i];
+          imgFn.load(name, function (img) {
+            n++;
+            _images[name] = img;
             if (n >= l) {
               if (callback) {
-                callback()
+                callback();
               }
             }
-          })
+          });
+        };
+
+        for (var i in images) {
+          _loop();
         }
       }
-    }
+    };
   }
-}
+};
 
-export default OnLoad
+export default OnLoad;
